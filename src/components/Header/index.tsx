7c7f4e16166e43e23a7d088563c7ec3359a9d6ac
Inio Idoate <idoate.127253@e.unavarra.es>
2022-07-07 20:52:56 -0300

    hide unnecesary user loged header options

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,21 +2,23 @@ import { Link } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
 
 export function Header() {
-  const { userIsLogged, handleLogOut } = useAuth()
+    const { userIsLogged, handleLogOut } = useAuth()
 
-  return (
-    <header>
-      <div>
-          <Link to="/" >TODO</Link>
-      </div>
-      <nav>
-        <ul>  
-            <li><Link to="/login">Entrar</Link></li>
-            <li><Link to="/register">Registrar</Link></li>
-            {userIsLogged && 
-            <li onClick={handleLogOut} style={{cursor: 'pointer'}}>Sair</li>}
-        </ul>
-      </nav>
-    </header>
-  )
+    return (
+        <header>
+            <div>
+                <Link to="/" >TODO</Link>
+            </div>
+            <nav>
+                <ul>
+                    {userIsLogged
+                        ?<li onClick={handleLogOut} style={{cursor: 'pointer'}}>Sair</li>
+                        :<>
+                            <li><Link to="/login">Entrar</Link></li>
+                            <li><Link to="/register">Registrar</Link></li>
+                        </>}
+                </ul>
+            </nav>
+        </header>
+    )
 }
